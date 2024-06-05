baedfbe17fb8dba1a08b13412fe373060ad54e2a
Youzhou Zhang <czhang@lcs.on.ca>
2024-06-05 09:46:40 -0400

    Initial Commit

---
+++ AssistiveTouch++/AssistiveTouch__App.swift
@@ -0,0 +1,17 @@
+//
+//  AssistiveTouch__App.swift
+//  AssistiveTouch++
+//
+//  Created by Yuzhou Zhang on 2024-06-05.
+//
+
+import SwiftUI
+
+@main
+struct AssistiveTouch__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssistiveTouch++/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AssistiveTouch++
+//
+//  Created by Yuzhou Zhang on 2024-06-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
