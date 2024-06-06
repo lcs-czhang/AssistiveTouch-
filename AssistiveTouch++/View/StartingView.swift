520e8840e1469a3b3f672325bdea35fa1af15cfb
Youzhou Zhang <czhang@lcs.on.ca>
2024-06-06 09:08:08 -0400

    Add an image for x, finished the first page of static view

---
+++ AssistiveTouch++/View/StartingView.swift
@@ -0,0 +1,41 @@
+//
+//  StartingView.swift
+//  AssistiveTouch++
+//
+//  Created by Yuzhou Zhang on 2024-06-05.
+//
+
+import SwiftUI
+
+struct StartingView: View {
+    var body: some View {
+        //NavigationStack {
+            VStack(alignment: .center, spacing: -20) {
+                Text("E-Tool")
+                    .font(.system(size: 80))
+                    .bold()
+                HStack(spacing: 0) {
+                    Text("Bo")
+                        .font(.system(size: 80))
+                        .bold()
+                    Image("custom.wrench.and.screwdriver.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 45)
+                        .padding(.top)
+                }
+                Spacer()
+                NavigationLink("Start", destination: tutorial())
+                    .font(.system(size: 80))
+                    .foregroundStyle(.black)
+                    .bold()
+            }
+        
+            .padding(50.0)
+        //}
+    }
+}
+
+#Preview {
+    StartingView()
+}
---
+++ AssistiveTouch++/View/tutorial.swift
@@ -0,0 +1,22 @@
+//
+//  tutorial.swift
+//  AssistiveTouch++
+//
+//  Created by Yuzhou Zhang on 2024-06-06.
+//
+
+import SwiftUI
+
+struct tutorial: View {
+    var body: some View {
+        //NavigationStack{
+        List{
+            
+        }
+        //}
+    }
+}
+
+#Preview {
+    tutorial()
+}
