071bb4e57bd23c8d2e13c17e32e7538b5af4c29e
Youzhou Zhang <czhang@lcs.on.ca>
2024-06-07 12:10:07 -0400

    Made SplashScreenView

--- AssistiveTouch++/View/SplashScreenView.swift
+++ AssistiveTouch++/View/SplashScreenView.swift
@@ -10,28 +10,34 @@ import SwiftUI
 struct SplashScreenView: View {
     var body: some View {
         //NavigationStack {
-            VStack(alignment: .center, spacing: -20) {
-                Text("E-Tool")
-                    .font(.system(size: 80))
-                    .bold()
-                HStack(spacing: 0) {
-                    Text("Bo")
+            ZStack {
+                Color.eToolBackground
+                    .ignoresSafeArea()
+                VStack(alignment: .center, spacing: -20) {
+                    Text("E-Tool")
                         .font(.system(size: 80))
                         .bold()
-                    Image("custom.wrench.and.screwdriver.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 45)
-                        .padding(.top)
+                        .foregroundStyle(Color.eToolText)
+                    HStack(spacing: 0) {
+                        Text("Bo")
+                            .font(.system(size: 80))
+                            .bold()
+                        Image("custom.wrench.and.screwdriver.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 45)
+                            .padding(.top)
+                    }
+                    .foregroundStyle(Color.eToolText)
+                    Spacer()
+                    NavigationLink("Start", destination: MenuView())
+                        .font(.system(size: 80))
+                        .foregroundStyle(Color.eToolText)
+                        .bold()
                 }
-                Spacer()
-                NavigationLink("Start", destination: tutorial())
-                    .font(.system(size: 80))
-                    .foregroundStyle(.black)
-                    .bold()
+                
+                .padding(50.0)
             }
-        
-            .padding(50.0)
         //}
     }
 }
