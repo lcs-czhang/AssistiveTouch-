6946c44a1309315df4db6930a27ee23b924800ef
Youzhou Zhang <czhang@lcs.on.ca>
2024-06-12 09:39:06 -0400

    Added a title and added the background

--- AssistiveTouch++/View/MenuView.swift
+++ AssistiveTouch++/View/MenuView.swift
@@ -9,46 +9,75 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        NavigationStack{
-            List{
-                NavigationLink {
-                    TranslationView()
-                } label: {
-                    Text("Translation")
+        //NavigationStack{
+            ZStack {
+                Color.eToolBackground
+                    .ignoresSafeArea()
+                VStack(alignment: .center, spacing: -20) {
+                    Text("E-Tool")
+                        .font(.system(size: 80))
+                        .bold()
+                        .foregroundStyle(Color.eToolText)
+                    HStack(spacing: 0) {
+                        Text("Bo")
+                            .font(.system(size: 80))
+                            .bold()
+                        Image("custom.wrench.and.screwdriver.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 45)
+                            .padding(.top)
+                    }
+                    .foregroundStyle(Color.eToolText)
+                    Spacer()
+                    List{
+                        NavigationLink {
+                            TranslationView()
+                        } label: {
+                            Text("Translation")
+                        }
+                        
+                        .foregroundStyle(.eToolText)
+                        
+                        NavigationLink {
+                            MagnifyingView()
+                        } label: {
+                            Text("Magnifying")
+                        }
+                        
+                        NavigationLink {
+                            WeatherReportView()
+                        } label: {
+                            Text("Weather Report")
+                        }
+                        
+                        NavigationLink {
+                            //CalculatorView()
+                        } label: {
+                            Text("Calculator")
+                        }
+                        
+                        NavigationLink {
+                            PaintingView()
+                        } label: {
+                            Text("Painting")
+                        }
+                        
+                        NavigationLink {
+                            VaultView()
+                        } label: {
+                            Text("Vault")
+                        }
+                        
+                    }
+                    .listStyle(.plain)
+                    .frame(height: 450)
                 }
                 
-                NavigationLink {
-                    MagnifyingView()
-                } label: {
-                    Text("Magnifying")
-                }
-                
-                NavigationLink {
-                    WeatherReportView()
-                } label: {
-                    Text("Weather Report")
-                }
-                
-                NavigationLink {
-                    CalculatorView()
-                } label: {
-                    Text("Calculator")
-                }
-                
-                NavigationLink {
-                    PaintingView()
-                } label: {
-                    Text("Painting")
-                }
-                
-                NavigationLink {
-                    VaultView()
-                } label: {
-                    Text("Vault")
-                }
-                
+                .padding(50.0)
             }
-        }
+            
+        //}
     }
 }
 
