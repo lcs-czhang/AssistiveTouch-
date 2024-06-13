c73a56157220f4341a504345c5b000e908d0da04
Youzhou Zhang <czhang@lcs.on.ca>
2024-06-13 08:57:50 -0400

    Created a folder for CalculatorView and CalculatorMemoryView

---
+++ AssistiveTouch++/View/FunctionView/CalculatorViewGroup/CalculatorMemoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorMemoryView.swift
+//  AssistiveTouch++
+//
+//  Created by Yuzhou Zhang on 2024-06-13.
+//
+
+import SwiftUI
+
+struct CalculatorMemoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalculatorMemoryView()
+}
