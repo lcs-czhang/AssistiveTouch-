584ccd03c61dded8d79233fe7ac0c87d2b2864b2
Youzhou Zhang <czhang@lcs.on.ca>
2024-06-13 09:55:21 -0400

    Added the Supabase framework.

---
+++ AssistiveTouch++/Helper/Supabase.swift
@@ -0,0 +1,8 @@
+//
+//  Supabase.swift
+//  AssistiveTouch++
+//
+//  Created by Yuzhou Zhang on 2024-06-13.
+//
+
+import Foundation
---
+++ AssistiveTouch++/Model/Memory.swift
@@ -0,0 +1,8 @@
+//
+//  Memory.swift
+//  AssistiveTouch++
+//
+//  Created by Yuzhou Zhang on 2024-06-13.
+//
+
+import Foundation
